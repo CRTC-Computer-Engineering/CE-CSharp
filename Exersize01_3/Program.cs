a2ade09de27e6381a6a8b7d360ce10ceb450e9d7
Joe S <31870999+KenwoodFox@users.noreply.github.com>
2020-03-05 10:34:15 -0500

    Exersize 3

---
+++ Exersize01_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exersize01_3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Provide a numerator..");
+            double numerator = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Provide a denominator..");
+            double denominator = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Here is your result: " + (SimpleMath.DivideTwoNumbers(numerator, denominator)).ToString());
+        }
+    }
+}
---
+++ Exersize01_3/SimpleMath.cs
@@ -0,0 +1,17 @@
+namespace Exersize01_3
+{
+    public class SimpleMath
+    {
+        public static int SumTwoNumbers(int one, int two)
+        // Takes two numbers and sums them
+        {
+            return (one + two); // Returns the result
+        }
+
+        public static double DivideTwoNumbers(double numerator, double denominator)
+        // Takes two numbers and divides them
+        {
+            return (numerator / denominator); // Returns the result
+        }
+    }
+}
