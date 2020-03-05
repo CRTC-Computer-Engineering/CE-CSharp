7bbbc352ac986feb649fb8631ef209c7761fa7b6
Joe S <31870999+KenwoodFox@users.noreply.github.com>
2020-03-05 10:40:48 -0500

    Exersize 4

---
+++ Exersize01_4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exersize01_4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Executing the following instructions:");
+            Console.WriteLine("-1 + 4 * 6 = " + Convert.ToString(-1 + 4 * 6));
+            Console.WriteLine("(35 + 5) % 7 = " + Convert.ToString((35 + 5) % 7));
+            Console.WriteLine("14 + -4 * 6 / 11 = " + Convert.ToString(14 + -4 * 6 / 11));
+            Console.WriteLine("2 + 15 / 6 * 1 - 7 % 2 = " + Convert.ToString(2 + 15 / 6 * 1 - 7 % 2));
+        }
+    }
+}
