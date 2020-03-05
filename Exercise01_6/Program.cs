3f48098f6f323f82443aae9c6d6e88f884d6d210
Joe S <31870999+KenwoodFox@users.noreply.github.com>
2020-03-05 10:53:47 -0500

    Very functional

---
+++ Exercise01_6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercise01_6
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Provide one of three nummahs to multiply.");
+            double nummah_one = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Provide two of three nummahs to multiply.");
+            double nummah_two = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Provide three of three nummahs to multiply.");
+            double nummah_three = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("Multiplying da nummahs");
+            Console.WriteLine("Result: " + Convert.ToString(nummah_one * nummah_two * nummah_three));
+        }
+    }
+}
