e842dbcfafb1e990e35fb3628e55d9567b99f64b
Joe S <31870999+KenwoodFox@users.noreply.github.com>
2020-03-05 10:29:11 -0500

    Finish problem two

--- Exersize01_2/Program.cs
+++ Exersize01_2/Program.cs
@@ -2,11 +2,16 @@
 
 namespace Exersize01_2
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Provide a number to be summed.");
+            int one = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Provide another number to be summed.");
+            int two = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Here is your result: " + (SimpleMath.SumTwoNumbers(one, two)).ToString());
         }
     }
 }
---
+++ Exersize01_2/SimpleMath.cs
@@ -0,0 +1,11 @@
+namespace Exersize01_2
+{
+    public class SimpleMath
+    {
+        public static int SumTwoNumbers(int one, int two)
+        // Takes two numbers and sums them
+        {
+            return (one + two); // Returns the result
+        }
+    }
+}
