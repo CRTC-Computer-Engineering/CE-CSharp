aa41a1f58f08b5abbcc10bbeaee627e4fab431dc
Joe S <31870999+KenwoodFox@users.noreply.github.com>
2020-03-06 09:54:00 -0500

    Add exersize 02_1

---
+++ Exercise02_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercise02_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            double first_num = console_methods.getUserNumber("First number: ");
+            double second_num = console_methods.getUserNumber("Second number: ");
+
+            Console.WriteLine(
+                Convert.ToString(first_num) + " + " + Convert.ToString(second_num) + " = " + Convert.ToString(first_num + second_num) + "\n" +
+                Convert.ToString(first_num) + " - " + Convert.ToString(second_num) + " = " + Convert.ToString(first_num - second_num) + "\n" +
+                Convert.ToString(first_num) + " * " + Convert.ToString(second_num) + " = " + Convert.ToString(first_num * second_num) + "\n" +
+                Convert.ToString(first_num) + " / " + Convert.ToString(second_num) + " = " + Convert.ToString(first_num / second_num) + "\n" +
+                Convert.ToString(first_num) + " % " + Convert.ToString(second_num) + " = " + Convert.ToString(first_num % second_num)
+            );
+        }
+    }
+}
