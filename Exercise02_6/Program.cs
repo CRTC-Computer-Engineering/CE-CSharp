5afde08fb3a9eb29795104be9a13010e23d258c6
Joe S <31870999+KenwoodFox@users.noreply.github.com>
2020-03-06 10:45:30 -0500

    exersize 2.6

---
+++ Exercise02_6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using JoesLib;
+
+namespace Exercise02_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double user_num = jonsole.getUserNumber("Enter a number: ");
+
+            for (int ay = 0; ay < 2; ay++)
+            {
+                for (int ayy = 0; ayy < 4; ayy++)
+                {
+                    Console.Write(Convert.ToString(user_num) + " ");
+                }
+                Console.Write("\n");
+                for (int ayyy = 0; ayyy < 4; ayyy++)
+                {
+                    Console.Write(Convert.ToString(user_num));
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
