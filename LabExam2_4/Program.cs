f04287ceccc049771112e0150cb0ee0d0f003d7c
Joe S <31870999+KenwoodFox@users.noreply.github.com>
2020-03-13 13:44:13 -0400

    Create problem 4

---
+++ LabExam2_4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LabExam2_3
+{
+    class Program
+    {
+        static void Main()
+        {
+            int current_multiple = 1;
+            int found_multiples = 0;
+            int target_multiples = 5;
+
+            while(found_multiples < target_multiples){
+                if (current_multiple % 5 == 0){
+                    Console.WriteLine("Last number was a multiple of five. Number was: " + Convert.ToString(current_multiple));
+                    found_multiples++;
+                }
+                current_multiple++;
+            }
+        }
+    }
+}
