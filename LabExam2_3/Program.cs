740b5aca8ee59dfa5f818a507fd321dced30a04f
Joe S <31870999+KenwoodFox@users.noreply.github.com>
2020-03-13 13:44:05 -0400

    Create problem 3

---
+++ LabExam2_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LabExam2_1
+{
+    class Program
+    {
+        static void Main() 
+        {
+            int i = 1;
+            int end = 5;
+
+            for (i = 0; i < end; i++)
+            {
+                Console.WriteLine("A Line. (" + Convert.ToString(i) + "/" + Convert.ToString(end) + ")");
+            }
+        }
+    }
+}
