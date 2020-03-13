17cd8fc43362876b9dc6a739b6d4a392d8c17977
Joe S <31870999+KenwoodFox@users.noreply.github.com>
2020-03-13 13:52:51 -0400

    Create problem 5

---
+++ LabExam2_5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace LabExam2_5
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i = 0;
+            int end = 10;
+            int[] ignored = {4, 7, 9};
+
+            while (i < end)
+            {
+                i++;
+                if (ignored.Contains(i))
+                {
+                    Console.WriteLine("Encountered number " + Convert.ToString(i) + " but number is ignored. Skipping number.");
+                    continue;
+                }
+                Console.WriteLine("Counting to " + Convert.ToString(end) + ", Current number is: " + Convert.ToString(i));
+            }
+        }
+    }
+}
