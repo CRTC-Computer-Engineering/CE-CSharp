41575ff876db609da3358e87a08184cdbee099ab
ZachrilageGameplays <50333523+ZachrilageGameplays@users.noreply.github.com>
2020-03-17 18:47:59 -0400

    Create Program.cs

---
+++ LabExam1/Collin_LabExam1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabExam01
+{
+    class Program
+    {
+        private static void DisplayMenu()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("  Cafe Diem Menu");
+            Console.WriteLine("         1  Prime Rib     - $13.99 per plate");
+            Console.WriteLine("         2  Fried Chicken - $8.49 per plate");
+            Console.WriteLine("         3  Baked Haddock - $9.99 per plate");
+            Console.WriteLine("         4  Soda          - $1.99 each");
+            Console.WriteLine("         5  Quit");
+            Console.Write("  Please make a selection: ");
+        }
+        private static void GetSelection()
+        {
+            int sel = Convert.ToInt32(Console.ReadLine());
+        }
+        private static void ValidateSelection(ref int sel)
+        { 
+            switch (sel)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                default:
+                    Console.WriteLine("  Sorry, there seems to have been an error. Please make another selection.");
+                    Console.Write("  Please make a selection: ");
+                    GetSelection();
+                    break;
+            }
+        }
+        static void Main(string[] args)
+        {
+            DisplayMenu();
+            GetSelection();
+            ValidateSelection();
+        }
+    }
+}
