eb95d8046dbdf71fe63f3f77ac2fadf11e93f6e5
Joe S <31870999+KenwoodFox@users.noreply.github.com>
2020-04-13 16:29:03 -0400

    Unstash

---
+++ LabExam3_5/Joe_LabExam3_5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using JoesLib;
+
+namespace Joe_LabExam3_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = Convert.ToInt16(jonsole.getUserNumber("Input number of desired rows: "));
+            Console.WriteLine(pyramid(rows));
+        }
+
+        private static String pyramid(int targetRows)
+        {
+            int i, j = 0;
+            string output = "";
+
+            for (i = 0; i < targetRows; i++)
+            {
+                for (j = 1; j <= targetRows - i; j++)
+                output = output + "\t";
+                for (j = 1; j <= 2 * i - 1; j++)
+                    output = output + "*\t";
+                output = output + "\n";
+            }
+            return output;
+        }
+    }
+}
